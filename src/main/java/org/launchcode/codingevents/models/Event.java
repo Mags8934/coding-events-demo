8e5a64538b99bd2abe9706a837ef527ca71d059d
Mags8934 <84347132+Mags8934@users.noreply.github.com>
2021-11-01 17:17:41 -0500

    Passing autograder tests

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -3,6 +3,7 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,12 +12,10 @@ import java.util.Objects;
 /**
  * Created by Chris Bay
  */
-@Entity
-public class Event {
 
-    @Id
-    @GeneratedValue
-    private int id;
+
+@Entity
+public class Event extends AbstractEntity {
 
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
@@ -31,14 +30,16 @@ public class Event {
 
     private EventType type;
 
+    public Event() {}
+
     public Event(String name, String description, String contactEmail, EventType type) {
+        this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
         this.type = type;
     }
 
-    public Event() {}
 
     public String getName() {
         return name;
@@ -72,25 +73,11 @@ public class Event {
         this.type = type;
     }
 
-    public int getId() {
-        return id;
-    }
 
     @Override
     public String toString() {
         return name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
